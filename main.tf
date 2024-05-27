a098df3b1ae190855d67ffef36a832022966ab1a
Sravika2509 <52756330+Sravika2509@users.noreply.github.com>
2024-05-27 21:20:55 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,16 +1,13 @@
-resource "random_id" "bucket_suffix" {
-  byte_length = var.random_suffix_length
-}
-
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket        = "${var.s3_bucket_name}-${random_id.bucket_suffix.hex}"
+    for_each = toset(var.s3_bucket_name)
+    bucket        = each.key
     tags          = var.tags
     force_destroy = var.force_destroy
     
 }
 resource "aws_s3_bucket_cors_configuration" "projects_bucket_cors" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
+  for_each = aws_s3_bucket.s3_bucket
+  bucket= each.key
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
@@ -20,16 +17,18 @@ resource "aws_s3_bucket_cors_configuration" "projects_bucket_cors" {
   }
 }
 
+
 resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
-  bucket        = aws_s3_bucket.s3_bucket.id
+  for_each = aws_s3_bucket.s3_bucket
+  bucket= each.key
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket        = aws_s3_bucket.s3_bucket.id
-
+  for_each = aws_s3_bucket.s3_bucket
+  bucket= each.key
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
@@ -38,7 +37,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_block_public_access" {
-  bucket        = aws_s3_bucket.s3_bucket.id
+  for_each = aws_s3_bucket.s3_bucket
+  bucket= each.key
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
